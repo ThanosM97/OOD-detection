0ede3201a4d31fc5d58f49bee97d4c6ad3b6c11d
Thanos Masouris <thanosmas97@gmail.com>
2022-04-13 18:41:15 +0200

    Add dataset class for CIFAR100

--- survae/data/datasets/image/unsupervised_wrappers/__init__.py
+++ survae/data/datasets/image/unsupervised_wrappers/__init__.py
@@ -1,4 +1,5 @@
 from .cifar10 import UnsupervisedCIFAR10
+from .cifar100 import UnsupervisedCIFAR100
 from .mnist import UnsupervisedMNIST
 from .fashion_mnist import UnsupervisedFashionMNIST
 from .svhn import UnsupervisedSVHN
